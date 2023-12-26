389035415f3cc8b5ddebedd4678351ad748ef96a
Merriidiian <meridian9695@mail.ru>
2023-12-26 10:00:46 +0300

    fix commission

--- CryptoTestApi.WebApi/DTOs/WalletDTO/WalletExchangeByCurrencyDTO.cs
+++ CryptoTestApi.WebApi/DTOs/WalletDTO/WalletExchangeByCurrencyDTO.cs
@@ -3,6 +3,6 @@
 public class WalletExchangeByCurrencyDTO
 {
     public record Request(Guid IdUser, Guid IdCurrencyWithdraw, Guid IdCurrencyReplenishment, double ExchangeRate,
-        double Sum, double? CommissionPercentage);
+        double Sum, double CommissionPercentage = 0.05);
     public record Response(string Result = "Успешно!");
 }
